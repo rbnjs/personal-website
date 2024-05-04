a2140e5c8584c1659e9550a386d8bf20c53e5367
Ruben Serradas <rubenserradas@gmail.com>
2024-05-04 09:51:30 +0200

    Add new layout plus favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    // any valid BCP 47-compatible language tag is supported
-    defaultLanguage: "en", // Required, this site uses "en"
-  });
-  eleventyConfig.addWatchTarget("index.css");
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+        // any valid BCP 47-compatible language tag is supported
+        defaultLanguage: "en", // Required, this site uses "en"
+    });
+    eleventyConfig.addWatchTarget("index.css");
+    eleventyConfig.addPassthroughCopy("img");
 };
