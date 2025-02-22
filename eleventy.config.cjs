729ed22a477e32e618a6fcd7ce786d707f682cfd
Ruben Serradas <rubenserradas@gmail.com>
2025-02-22 14:53:48 +0100

    add fonts

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addWatchTarget("index.css");
 	eleventyConfig.addPassthroughCopy("img");
+	eleventyConfig.addPassthroughCopy("fonts");
 	eleventyConfig.addFilter("limit", function(array, limit) {
 		return array.slice(0, limit);
 	});
